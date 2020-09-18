1ee6b7416e763a1839b374bf4c6a79e823bef84e
Amit Gupta <amit@gprof.com>
2020-09-17 19:52:54 -0700

    Initial Commit

---
+++ Learner Permit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learner Permit
+//
+//  Created by Amit Gupta on 9/3/20.
+//  Copyright © 2020 AI Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
